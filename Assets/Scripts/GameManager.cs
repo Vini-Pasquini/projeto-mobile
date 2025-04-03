f8b8e9891f14f26b5f304447eac13105a1236bf5
yara <yararibr@gmail.com>
2025-04-03 13:01:05 -0300

    Spawnpoints aleatorios
    Suporte para múltiplos spawnpoints, player é jogado para um aleatório no inicio da partida.
    Porta do spawnpoint fechando com cutscene.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,13 +6,29 @@ public class GameManager : IPersistentSingleton<GameManager>
     private LibrasSign[] _librasLetterSigns;
     private LibrasSign[] _librasWordSigns;
 
-    private PlayerController player;
+    [SerializeField] private GameObject[] spawnpoints;
+
+    public GameObject door;
+    private GameObject player;
+    private PlayerController playerCtrl;
 
     protected override void Awake()
     {
         base.Awake();
         this._librasLetterSigns = Resources.LoadAll<LibrasSign>("LibrasSigns/Letters");
-        player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+        player = GameObject.FindWithTag("Player");
+        playerCtrl = player.GetComponent<PlayerController>();
+        door = GameObject.FindWithTag("Spawndoor");
+        door.SetActive(false);
+
+        this.spawnpoints = GameObject.FindGameObjectsWithTag("Spawnpoint");
+    }
+
+    private void Start()
+    {
+        int spawn = Random.Range(0, spawnpoints.Length);
+        player.transform.position = spawnpoints[spawn].transform.position;
+        Debug.Log("Player nasceu no spawn " + spawn);
     }
 
     // TODO: implementar track de dup
@@ -25,6 +41,6 @@ public class GameManager : IPersistentSingleton<GameManager>
     {
         Debug.Log("Encontrou o Minotauro");
         GameObject.Find("Canvas").GetComponent<UIController>().ActivateGameOver();
-        player.SetIsActive(false);
+        playerCtrl.SetIsActive(false);
     }
 }
