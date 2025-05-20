1f9ea11803e7a7cba4c625eb420224ffa528e7d7
yara <yararibr@gmail.com>
2025-05-20 18:06:28 -0300

    Animacoes e UI
    Adicionados sprites finais de UI e libras;
    Adicionados e implementados sprites e animações finais de personagens e da barra de vida;

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public MovementJoystick joystick;
+    public HealthBar healthBar;
 
     [SerializeField] private int health = 3;
     [SerializeField] private float speed = 5f;
@@ -10,10 +11,13 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveDirection;
 
     private Rigidbody2D rb;
+    private Animator myAnimator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponentInChildren<Animator>();
+        healthBar.SetHealth(health);
     }
 
     void FixedUpdate()
@@ -32,11 +36,32 @@ public class PlayerController : MonoBehaviour
             moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
             if (moveDirection != Vector2.zero)
+            {
                 rb.linearVelocity = moveDirection * speed;
+
+                myAnimator.SetBool("WalkDown", true);
+
+                if (moveDirection.y < 0)
+                {
+                    myAnimator.SetBool("WalkUp", false);
+                }
+                else if (moveDirection.y > 0)
+                {
+                    myAnimator.SetBool("WalkDown", false);
+                    myAnimator.SetBool("WalkUp", true);
+                }
+            }
+            else
+            {
+                myAnimator.SetBool("WalkDown", false);
+                myAnimator.SetBool("WalkUp", false);
+            }
         }
         else
         {
             rb.linearVelocity = Vector2.zero;
+            myAnimator.SetBool("WalkDown", false);
+            myAnimator.SetBool("WalkUp", false);
         }
     }
 
@@ -44,6 +69,7 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log("Tomou dano");
         health--;
+        healthBar.SetHealth(health);
 
         if (health <= 0)
         {
