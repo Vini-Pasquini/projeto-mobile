7b33f4dcd8ce00d2f79cb34f574a68be3d08a9dc
yara <yararibr@gmail.com>
2025-05-22 17:02:29 -0300

    Finalizacao Playtest 2
    Qualidade de vida

--- Assets/Scripts/Player/CameraController.cs
+++ Assets/Scripts/Player/CameraController.cs
@@ -7,6 +7,7 @@ public class CameraController : MonoBehaviour
     public Transform target;
     public float smoothing;
     public float yOffset;
+    private float cameraYOffset = 0f;
 
     public Vector2 maxPos;
     public Vector2 minPos;
@@ -15,12 +16,17 @@ public class CameraController : MonoBehaviour
     {
         if (transform.position != target.position)
         {
-            Vector3 targetPos = new Vector3(target.position.x, target.position.y + yOffset, transform.position.z);
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y + cameraYOffset, transform.position.z);
 
             targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);
             targetPos.y = Mathf.Clamp(targetPos.y, minPos.y, maxPos.y);
 
             transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
         }
+
+        if (Camera.main.orthographicSize > 1)
+            cameraYOffset = yOffset;
+        else
+            cameraYOffset = 0f;
     }
 }
